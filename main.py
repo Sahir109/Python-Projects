402bd26aa435a4642e13256d5b4e665483447ea6
Zeeshan Yaqoob <zeeshanyaqoob999@gmail.com>
2023-07-11 04:40:51 -0700

    Commit message

---
+++ main.py
@@ -0,0 +1,21 @@
+# Python program to convert
+# text to speech
+
+# import the required module from text to speech conversion
+import win32com.client
+
+# Calling the Dispatch method of the module which
+# interact with Microsoft Speech SDK to speak
+# the given input from the keyboard
+
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+while(True):
+    s = input("enter what you want me to speak: ")
+    if(s=="q"):
+        speaker.Speak(f"Good Bye it was nice talking to you")
+        break
+    speaker.Speak(f"{s}")
+
+
+# To stop the program press
+# CTRL + Z
