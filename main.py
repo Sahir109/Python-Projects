20cfbd12025a65bc9e3abdac9206b4630af72e66
Zeeshan Yaqoob <91457819+Sahir109@users.noreply.github.com>
2023-09-03 10:59:56 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
-# Robo Code in Python
-# import the required module from text to speech conversion
-import win32com.client
+import shutil
+import os
+import subprocess
 
-# Calling the Dispatch method of the module which
-# interact with Microsoft Speech SDK to speak
-# the given input from the keyboard
+usb_drive_letter = "G:"  # Update with the appropriate drive letter of your USB drive
+destination_folder = "pdf"  # Name of the destination folder to store the copied PDF files
 
-speaker = win32com.client.Dispatch("SAPI.SpVoice")
-while(True):
-    s = input("enter what you want me to speak: ")
-    if(s=="q"):
-        speaker.Speak(f"Good Bye it was nice talking to you")
-        break
-    speaker.Speak(f"{s}")
+def copy_pdf_files(source_folder, destination_folder):
+    for root, _, files in os.walk(source_folder):
+        for file in files:
+            if file.endswith(".pdf"):
+                source_path = os.path.join(root, file)
+                destination_path = os.path.join(destination_folder, file)
+                shutil.copy(source_path, destination_path)
 
-# To stop the program Enter q
+def monitor_usb_drive():
+    command = f'WMIC LOGICALDISK WHERE "DriveType=2" GET DeviceID'
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    output, _ = process.communicate()
+    drive_letters = [drive.decode().strip() for drive in output.split()[1:]]
+
+    while True:
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        new_output, _ = process.communicate()
+        new_drive_letters = [drive.decode().strip() for drive in new_output.split()[1:]]
+
+        # Check if any new drive has been inserted
+        inserted_drives = list(set(new_drive_letters) - set(drive_letters))
+        if inserted_drives:
+            for drive_letter in inserted_drives:
+                drive_path = os.path.join(drive_letter, "")
+                if os.path.isdir(drive_path):
+                    source_folder = drive_path
+                    destination_folder_path = os.path.join(destination_folder, "")
+                    if not os.path.exists(destination_folder_path):
+                        os.makedirs(destination_folder_path)
+                    copy_pdf_files(source_folder, destination_folder_path)
+                    print(f"PDF files from {source_folder} have been copied to {destination_folder_path}")
+
+        drive_letters = new_drive_letters
+
+# Start monitoring the USB drive
+monitor_usb_drive()
